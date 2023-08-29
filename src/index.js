0659671334bfae5b0b00527b98cac0b59be54cd9
Denis Dias <denistodias1@gmail.com>
2023-08-29 15:54:56 -0300

    API

--- src/index.js
+++ src/index.js
@@ -22,9 +22,6 @@ server.listen(process.env.PORT,
     () => 	console.log(`API ONLINE na Porta ${process.env.PORT} seja bem-vindo!`));
 
 
-    server.get('/ping', (req, resp) =>{
-        resp.send('pong')
-    })
     
 
 
